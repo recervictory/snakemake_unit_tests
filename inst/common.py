ba49a101a881a7b3703a4d57581a2bc9976425bf
Bari J. Ballew <bari.ballew@gmail.com>
2021-03-25 11:34:44 -0400

    feat: ignore files with listed extensions, add unit tests
    re #16

--- inst/common.py
+++ inst/common.py
@@ -8,6 +8,9 @@ import os
 import pytest
 import gzip
 
+exclude_ext = [".tbi", ".html"]
+# TODO: Read in a list of extensions to exclude from the config.  See issue #16.
+
 class OutputChecker:
     def __init__(self, data_path, expected_path, workdir):
         self.data_path = data_path
@@ -35,7 +38,7 @@ class OutputChecker:
                     continue
                 if "/logs/" in str(f):
                     continue
-                if str(f).endswith(".tbi"):
+                if str(f).endswith(tuple(exclude_ext)):
                     continue
                 if f in expected_files:
                     self.compare_files(self.workdir / f, self.expected_path / f)
@@ -53,22 +56,23 @@ class OutputChecker:
 
     def compare_files(self, generated_file, expected_file):
         if str(generated_file).lower().endswith((".vcf.gz", ".vcf")):
-            gen = remove_headers(generated_file)
-            exp = remove_headers(expected_file)
+            gen = process_file(generated_file)
+            exp = process_file(expected_file)
             assert gen == exp
         else:
             sp.check_output(["cmp", generated_file, expected_file])
 
-def remove_headers(infile):
-    n = []
+def process_file(infile):
     if str(infile).lower().endswith(".gz"):
         with gzip.open(infile, mode="rt") as f:
-            for l in (l for l in f if not l.startswith("##")):
-                n.append(l)
+            n = remove_headers(f)
     else:
         with open(infile, "r") as f:
-            for l in (l for l in f if not l.startswith("##")):
-                n.append(l)
+            n = remove_headers(f)
     return n
 
-# open an issue for config list of file endings to ignore
+def remove_headers(f):
+    n = []
+    for l in (l for l in f if not l.startswith("##")):
+        n.append(l)
+    return n
---
+++ inst/test_common.py
@@ -0,0 +1,8 @@
+import common
+import pytest
+
+def test_remove_headers():
+    test_in = ["##header row1", "## header row2", "#CHROM\tPOS\tID\tREF\tALT", "chr1\t123\t.\tA\tT"]
+    test_out = common.remove_headers(test_in)
+    expected_out = ["#CHROM\tPOS\tID\tREF\tALT", "chr1\t123\t.\tA\tT"]
+    assert test_out == expected_out
