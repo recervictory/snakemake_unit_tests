e1e6b0a38fcec0fd299c1a6d41ba3a932e82eed4
cpalmer718 <cameron.palmer@54gene.com>
2022-02-05 13:58:15 -0500

    ci: run individual docker commands

--- .github/workflows/build_test_commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mamba env create -f environment-linux.yaml
-conda activate snakemake_unit_tests-env
-./generate.bash
-./configure --with-boost=$CONDA_PREFIX --with-boost-libdir=$CONDA_PREFIX/lib
-make -j2
-make -j2 check
-./test_suite.out
