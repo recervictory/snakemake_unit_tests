d5b5041c4ee61750ac9404fc10fa3a845891b912
cpalmer718 <cameron.palmer@54gene.com>
2022-06-05 10:08:41 -0400

    docs: bump initial readthedocs content to correct version number

--- readthedocs/source/conf.py
+++ readthedocs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Cameron Palmer, Bari Ballew"
 author = "Cameron Palmer, Bari Ballew"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
